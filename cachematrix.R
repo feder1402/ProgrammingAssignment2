5c325f6450d0779b10f72601a18782ef9729f833
Federico Raggi <federicoraggi@hotmail.com>
2014-04-24 14:05:32 -0700

    Implemented makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# ================================================================================
+# makeCacheMatrix: Creates a special object that can cache the inverse of a matrix
+# ================================================================================
+# Args:
+#   x: Matrix whose inverse wil be cached
+#
+# Returns:
+#   A special object that allows caching x's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  # Sets a new value for the wrapped matrix
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  # Returns the value of the wrapped matrix
+  get <- function() {
+    x
+  }
+  
+  # Sets the inverse of the matrix to cache
+  setinverse <- function(inverse) {
+    inv <<- inverse
+  }
+  
+  # Gets the cached inverse of the matrix
+  getinverse <- function() {
+    inv
+  }
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+# =================================================================================================
+# cacheSolve: Computes the inverse of a matrix m where m is wrapped by a special object returned by 
+#             makeCacheMatrix(m)
+# =================================================================================================
+#
+# Args:
+#   x: The special object returned by makeCacheMatrix(m)
+#
+# Returns:
+#   The inverse of the matrix m 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  
+  if (!is.null(inv)) {
+    message("getting cached data")
+  } else {
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+  }
+  
+  return (inv)
 }
